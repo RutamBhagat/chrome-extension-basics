f3c6c40c692dc192c552e32014a63a21aa8ef560
RutamBhagat <rutambhagat@gmail.com>
2024-09-09 14:21:05 +0530

    feat(chrome-extension-boilerplate-vite): add notifications

--- chrome-extension-boilerplate-react-vite/chrome-extension/src/background/index.ts
+++ chrome-extension-boilerplate-react-vite/chrome-extension/src/background/index.ts
@@ -9,13 +9,6 @@ exampleThemeStorage.get().then(theme => {
 console.log('background loaded');
 console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
 
-// let time = 0;
-
-// setInterval(() => {
-//   time += 1;
-//   console.log('Time: ', time);
-// }, 1000);
-
 chrome.alarms.create('timer', {
   periodInMinutes: 1 / 60,
 });
@@ -26,5 +19,14 @@ chrome.alarms.onAlarm.addListener(alarm => {
     time += 1;
     chrome.storage.local.set({ timer: time });
     chrome.action.setBadgeText({ text: time.toString() });
+
+    if (time % 1000 === 0) {
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: '/icon-34.png',
+        title: 'Chrome Timer Extension',
+        message: `1000 seconds have passed, timer is at ${time}`,
+      });
+    }
   });
 });
