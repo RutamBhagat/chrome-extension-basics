f1615cc59dcf80c08fe3f2dd7afb6f1a598e8f39
RutamBhagat <rutambhagat@gmail.com>
2024-09-09 10:37:33 +0530

    feat(chrome-extension-boilerplate-vite): comment out scripts

--- chrome-extension-boilerplate-react-vite/chrome-extension/manifest.js
+++ chrome-extension-boilerplate-react-vite/chrome-extension/manifest.js
@@ -1,5 +1,5 @@
-import fs from 'node:fs';
 import deepmerge from 'deepmerge';
+import fs from 'node:fs';
 
 const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
@@ -38,25 +38,25 @@ const manifest = deepmerge(
       default_popup: 'popup/index.html',
       default_icon: 'icon-34.png',
     },
-    chrome_url_overrides: {
-      newtab: 'new-tab/index.html',
-    },
+    // chrome_url_overrides: {
+    //   newtab: 'new-tab/index.html',
+    // },
     icons: {
       128: 'icon-128.png',
     },
     content_scripts: [
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content-ui/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        css: ['content.css'], // public folder
-      },
+      // {
+      //   matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      //   js: ['content/index.iife.js'],
+      // },
+      // {
+      //   matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      //   js: ['content-ui/index.iife.js'],
+      // },
+      // {
+      //   matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      //   css: ['content.css'], // public folder
+      // },
     ],
     devtools_page: 'devtools/index.html',
     web_accessible_resources: [
