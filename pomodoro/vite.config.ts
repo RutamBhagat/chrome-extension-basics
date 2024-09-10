74240ba4235ffbcd02721c1c14d0e97f547c37cb
RutamBhagat <rutambhagat@gmail.com>
2024-09-10 23:08:18 +0530

    feat(pomodoro): add shadcn

--- pomodoro/vite.config.ts
+++ pomodoro/vite.config.ts
@@ -1,9 +1,15 @@
 import { crx } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 import manifest from "./manifest.json";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
